50775f2099b3e1498179b0096499f5dca4769894
christian <christianbiringanine22@gmail.com>
2022-12-16 12:51:31 +0200

    Generate the friend models and set the association

---
+++ db/migrate/20221216105040_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :linked_in
+      t.string :twitter
+      t.belongs_to :User, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
