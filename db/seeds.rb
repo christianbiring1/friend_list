6281151ac7005c887e53b3173927e63abeb264a0
christian <christianbiringanine22@gmail.com>
2022-12-16 16:07:14 +0200

    Database setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create!([
+  {
+    first_name: 'Christian',
+    last_name: 'Biringanine',
+    email: 'christianbiringanine22@gmail.com'
+  },
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'jdoe@gmail.com'
+  },
+  {
+    first_name: 'Jane',
+    last_name: 'Smith',
+    email: 'smithjane@gmail.com'
+  },
+  {
+    first_name: 'Oliviera',
+    last_name: 'Kim',
+    email: 'olikim22@gmail.com'
+  },
+  {
+    first_name: 'Brown',
+    last_name: 'MCclurkin',
+    email: 'brownymc@gmail.com'
+  },
+])
+
+
+friends = Friend.create!([
+  {
+    first_name: 'Allan',
+    last_name: 'Smith',
+    email: 'allan@gmail.com',
+    linked_in: 'https//:www.linked-in.com-allan-smith',
+    twitter: '@allansmith',
+    user_id: 1
+  },
+  {
+    first_name: 'Ruby',
+    last_name: 'Gonzalez',
+    email: 'gonzar@gmail.com',
+    linked_in: 'https//:www.linked-in.com-gonzalez-ruby',
+    twitter: '@rubtgonzal',
+    user_id: 3
+  },
+  {
+    first_name: 'Ruby',
+    last_name: 'Gonzalez',
+    email: 'gonzar@gmail.com',
+    linked_in: 'https//:www.linked-in.com-gonzalez-ruby',
+    twitter: '@rubtgonzal',
+    user_id: 2
+  }
+])
+
+
